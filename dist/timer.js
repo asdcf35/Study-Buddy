65a75ae0568559cde24f52fc3ded60adcfc0eb69
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2022-02-05 18:27:25 -0500

    click

--- dist/timer.js
+++ dist/timer.js
@@ -1,10 +1,15 @@
+
 clicked = 0;
+function clickIncrease(){
+    clicked++;
+    return clicked;
+}
 let PomodoroTimer = Vue.createApp({
     data() {
         return {
             minutes: 25,
             seconds: 0,
-            isPaused: false,
+            timerResumeToPause: "Pause",
         };
     },
     mounted() {
@@ -30,28 +35,28 @@ let PomodoroTimer = Vue.createApp({
             this.minutes = 25;
             this.seconds = 0;
         },
-        pause() {
-            // pause the timer
-            this.isPaused = true;
-        },
-        resume() {
-            clicked++;
-            for(i==0; i<; i++){
-                if (this.seconds == 0) {
-                    this.seconds = 60;
-                    this.minutes--;
-                }
-                if(clicked == 1){
-                    this.seconds -= clicked;
-                } else{
-                    clicked == 1;
-                if(this.isPaused == True){
-                    this.isPaused = false;
-                    break;
-                }
-                }
-
-            }
+        pause() {           
+            //pause the timer and resume it when clicked
+            if (this.timerResumeToPause === "Pause") {
+                clearInterval(this.timer);
+                this.timerResumeToPause = "Resume";
+            } else {
+                this.timer = setInterval(() => {
+                    // do not go below 0
+                    if (this.seconds === 0) {
+                        if (this.minutes === 0) {
+                           clearInterval(this.timer);
+                           alert('Time is up!');
+                        } else {
+                            this.minutes--;
+                            this.seconds = 59;
+                        }
+                    } else {
+                        this.seconds--;
+                    }
+                }, 1000);
+                this.timerResumeToPause = "Pause";
+        }
         },
         breakTime() {
             this.minutes = 5;
